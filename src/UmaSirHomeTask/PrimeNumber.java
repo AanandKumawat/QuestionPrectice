411822b906cf8f03311e0cae2bd14cb44672fa7e
aanand kumawat <aanandkumawat1232@gmail.com>
2025-06-19 15:18:21 +0530

    primer number me changes

--- src/UmaSirHomeTask/PrimeNumber.java
+++ src/UmaSirHomeTask/PrimeNumber.java
@@ -5,7 +5,7 @@ public class PrimeNumber {
         if (num<2){
             return true;
         }
-        for (int i=2;i<num;i++){
+        for (int i=2;i<num/2;i++){
             if (num%i==0){
                 return false;
             }
